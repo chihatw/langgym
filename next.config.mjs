7a2253ac646bd0bbb839ef9064de351f2f07a4d4
chihatw <chiha.tw@gmail.com>
2024-04-27 13:01:06 +0900

    add hostname to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ hostname: '127.0.0.1' }],
+    remotePatterns: [
+      { hostname: '127.0.0.1' },
+      { hostname: 'ifhrlwhlgpgzpmwdonjo.supabase.co' },
+    ],
   },
 };
 
