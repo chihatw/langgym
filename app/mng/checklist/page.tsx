768ff8ee9a2a139991bf5fce4f94b37b5aa5b5c1
chihatw <chiha.tw@gmail.com>
2024-07-25 10:40:00 +0900

    add checklist

---
+++ app/mng/checklist/page.tsx
@@ -0,0 +1,9 @@
+import MngChecklistForm from '@/features/checklist/components/MngChecklistForm';
+
+type Props = {};
+
+const ChecklistPage = (props: Props) => {
+  return <MngChecklistForm />;
+};
+
+export default ChecklistPage;
