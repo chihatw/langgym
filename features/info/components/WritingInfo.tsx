d77652ad28e716db959497cd7b3b51bb1dc47f49
chihatw <chiha.tw@gmail.com>
2024-08-23 15:44:31 +0900

    update writing info

--- features/info/components/WritingInfo.tsx
+++ features/info/components/WritingInfo.tsx
@@ -7,7 +7,7 @@ const WritingInfo = (props: Props) => {
         ・8/30<span className='text-red-500'> 作文不用日文</span>
       </div>
       <div className='pl-4 text-sm text-gray-500 grid'>
-        <div>為了讓對方做課前準備，用中文寫作文就好</div>
+        <div>為了課前備課與對話練習，用中文寫作文就好</div>
       </div>
     </div>
   );
