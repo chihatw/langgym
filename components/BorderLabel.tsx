0c44ca39d93fa1ffbccbb190d9c37b6ff1f625cf
chihatw <chiha.tw@gmail.com>
2025-05-26 15:47:03 +0900

    fix: RootPage のラベルの文字修正 Fixes #7

--- components/BorderLabel.tsx
+++ components/BorderLabel.tsx
@@ -2,7 +2,7 @@ type Props = { label: string };
 
 const BorderLabel = ({ label }: Props) => {
   return (
-    <div className='  border-l-8 border-gray-800 rounded py-2 bg-gray-800 bg-opacity-10 text-sm'>
+    <div className='  border-l-8 border-gray-800 rounded py-2 bg-gray-800 bg-opacity-10 text-sm text-white'>
       <span className='pl-2'>{label}</span>
     </div>
   );
